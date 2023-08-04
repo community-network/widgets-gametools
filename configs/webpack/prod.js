1c6df5129d74f42c5ece2583d1f9dba14616ed13
Zefanja Jobse <zefanjaj@gmail.com>
2023-08-05 00:22:21 +0200

    sw

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,6 +1,7 @@
 // production config
 const { merge } = require("webpack-merge");
 const { resolve } = require("path");
+const { GenerateSW } = require("workbox-webpack-plugin");
 
 const commonConfig = require("./common");
 
@@ -16,4 +17,28 @@ module.exports = merge(commonConfig, {
     react: "React",
     "react-dom": "ReactDOM",
   },
+  plugins: [
+    new GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+          handler: "CacheFirst",
+        },
+        {
+          urlPattern: new RegExp(
+            "^https://fonts.(?:googleapis|gstatic).com/(.*)",
+          ),
+          handler: "CacheFirst",
+        },
+        {
+          urlPattern: new RegExp("^https://cdn.jsdelivr.net/(.*)"),
+          handler: "CacheFirst",
+        },
+        {
+          urlPattern: new RegExp("^https://unpkg.com/(.*)"),
+          handler: "CacheFirst",
+        },
+      ],
+    }),
+  ],
 });
