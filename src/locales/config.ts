f0b54e25755e35fcba191789fc146b78e79e563c
Zefanja Jobse <zefanjaj@gmail.com>
2023-10-26 22:56:35 +0200

    .

--- src/locales/config.ts
+++ src/locales/config.ts
@@ -1,31 +1,14 @@
 import i18n from "i18next";
-import translationEN from "./languages/en-US.json";
-import translationTR from "./languages/tr-TR.json";
-import translationCH from "./languages/zh-CN.json";
-import translationNL from "./languages/nl-NL.json";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import resourcesToBackend from "i18next-resources-to-backend";
 
-export const resources = {
-  "en-US": {
-    translation: translationEN,
-  },
-  "tr-TR": {
-    translation: translationTR,
-  },
-  // "ru-RU": {
-  //   translation: translationRU,
-  // },
-  "zh-CN": {
-    translation: translationCH,
-  },
-  "nl-NL": {
-    translation: translationNL,
-  },
-} as const;
-
-i18n.use(initReactI18next).use(LanguageDetector).init({
-  resources,
+i18n
+.use(
+  resourcesToBackend((language: string) => 
+    import(`./languages/${language}.json`)
+  ),
+).use(initReactI18next).use(LanguageDetector).init({
   fallbackLng: "en-US",
 });
 
