c9c0121494f6347080d1476203c6689b77a09aff
iiTzArcur <zefanjaj@gmail.com>
2024-09-22 15:20:28 +0200

    add small version of server detailed for amg

--- src/components/Servers.tsx
+++ src/components/Servers.tsx
@@ -292,6 +292,134 @@ function DetailedDefaults({
   );
 }
 
+
+export function SmallServerBox(): React.ReactElement {
+  const { t } = useTranslation();
+  const match = useMatch("/servers/small/:gameid/:type/:sname/:platform");
+  const gameId = match.params.gameid;
+  const serverName = unescape(match.params.sname).replaceAll('"', '\\"');
+  const query = new URLSearchParams(useLocation().search);
+  const zoomQuery = query.get("zoom");
+
+  const {
+    isLoading: loading,
+    isError: error,
+    data: stats,
+  } = useQuery({
+    queryKey: ["servers" + gameId + serverName + match.params.platform],
+    queryFn: () =>
+      GetStats.server({
+        game: gameId,
+        getter: match.params.type,
+        serverName: serverName,
+        lang: getLanguage(),
+        platform: match.params.platform,
+        with_ownername: false,
+      }),
+    retry: 1,
+  });
+  if (loading) {
+    return (
+      <SmallDefaults
+        match={match}
+        zoom={zoomQuery ?? 100}
+        gameId={gameId}
+      />
+    );
+  }
+
+  if (error || stats === undefined) {
+    return (
+      <SmallDefaults
+        match={match}
+        zoom={zoomQuery ?? 100}
+        gameId={gameId}
+      />
+    );
+  }
+  return (
+    <a
+      className={styles.BigServer}
+      href={`https://gametools.network/servers/${gameId}/name/${encodeURIComponent(
+        match.params.sname,
+      )}/${match.params.platform}`}
+      target="_blank"
+      style={calculateZoomStyle(zoomQuery ?? 100)}
+      rel="noreferrer"
+    >
+      <div
+        className={styles.BigServerImage}
+        style={{
+          backgroundImage: `url("${stats?.currentMapImage || stats?.mapImage}")`,
+        }}
+      >
+        <div className={styles.blur}>
+          {stats?.favorites && (
+            <p className={styles.serverFavorites}>
+              &#9733; {stats?.favorites}
+            </p>
+          )}
+        </div>
+      </div>
+      <div style={{ display: "grid" }}>
+        <div className={styles.Row}>
+          <h4 className={styles.Title}>{t("server.players")}</h4>
+          <p className={styles.Description}>
+            {stats?.playerAmount}/{stats?.maxPlayerAmount}
+            {stats?.maxPlayers}
+          </p>
+        </div>
+        {gameId !== "bf2042" && dice.includes(gameId) && (
+          <div className={styles.Row}>
+            <h4 className={styles.Title}>{t("server.queue")}</h4>
+            <p className={styles.Description}>{stats?.inQueue}/10</p>
+          </div>
+        )}
+      </div>
+    </a>
+  );
+}
+
+function SmallDefaults({
+  match,
+  gameId,
+  zoom,
+}: Readonly<{
+  match: PathMatch<"gameid" | "type" | "sname" | "platform">;
+  zoom: number | string;
+  gameId: string;
+}>): React.ReactElement {
+  const { t } = useTranslation();
+  const params = GetStats.constructParamStr({
+    search: match.params.sname,
+    game: gameId,
+    platform: match.params.platform,
+  });
+  return (
+    <a
+      className={styles.BigServer}
+      href={`https://gametools.network/servers${params}`}
+      target="_blank"
+      style={calculateZoomStyle(zoom)}
+      rel="noreferrer"
+    >
+      <div className={styles.BigServerImage} />
+      <div style={{ display: "grid" }}>
+        <div className={styles.Row}>
+          <h4 className={styles.Title}>{t("server.players")}</h4>
+          <p className={styles.Description}>0/0</p>
+        </div>
+        {gameId !== "bf2042" && dice.includes(gameId) && (
+          <div className={styles.Row}>
+            <h4 className={styles.Title}>{t("server.queue")}</h4>
+            <p className={styles.Description}>0/10</p>
+          </div>
+        )}
+      </div>
+    </a>
+  );
+}
+
 export default function Routing(): React.ReactElement {
   return (
     <Routes>
@@ -306,6 +434,10 @@ export default function Routing(): React.ReactElement {
       <Route
         path="/detailed/:gameid/:type/:sname/:platform"
         element={<DetailedServerBox />}
+      />
+      <Route
+        path="/small/:gameid/:type/:sname/:platform"
+        element={<SmallServerBox />}
       />
     </Routes>
   );
